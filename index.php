e57138ead8f9efdbfd3f27d8a576dd7355777f4c
Thien <imagineworld14@gmail.com>
2023-03-13 23:53:13 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Web bán phụ kiện điện tử</title>
+</head>
+<body>
+    <div class="wrapper">
+
+        <div class="header"></div>
+        <div class="menu">
+            <ul class="list_menu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="list.php">Danh mục sản phẩm</a></li>
+                <li><a href="giohang.php">Giỏ hàng</a></li>
+                <li><a href="#">Tin tức</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+        <div id="main">
+            <div class="sidebar">
+                Sidebar
+            </div>
+            <div class="maincontent">
+                Main content
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+
+    </div>
+</body>
+</html>
