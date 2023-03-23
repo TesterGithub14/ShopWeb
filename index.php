226564bb8ef57d708bd364c9a2f4dc3586229cff
Thien <imagineworld14@gmail.com>
2023-03-23 13:02:46 +0700

    update 1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Web bán phụ kiện điện tử</title>
+    <title>Web bán linh kiện máy tính</title>
 </head>
 <body>
     <div class="wrapper">
@@ -14,18 +14,32 @@
         <div class="menu">
             <ul class="list_menu">
                 <li><a href="index.php">Trang chủ</a></li>
-                <li><a href="list.php">Danh mục sản phẩm</a></li>
-                <li><a href="giohang.php">Giỏ hàng</a></li>
+                <li><a href="#">Danh mục sản phẩm</a></li>
+                <li><a href="#">Giỏ hàng</a></li>
                 <li><a href="#">Tin tức</a></li>
                 <li><a href="#">Liên hệ</a></li>
             </ul>
         </div>
         <div id="main">
             <div class="sidebar">
-                Sidebar
+                <ul class="list_sidebar">
+                    <li><a href="#">PC case</a></li>
+                    <li><a href="#">Tai nghe</a></li>
+                    <li><a href="#">Màn hình</a></li>
+                    <li><a href="#">VGA</a></li>
+                    <li><a href="#">Bàn phím</a></li>
+                    <li><a href="#">Chuột</a></li>
+                </ul>
             </div>
+            
             <div class="maincontent">
-                Main content
+                <ul class="product_list">
+                    <li>
+                        <img src="images/case.png">
+                        <p>Tên sản phẩm: Thùng custom Galaxy</p>
+                        <p>Giá: 16000000 vnd</p>
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="clear"></div>
