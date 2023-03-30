f085680854a4dc6376b5d4828369290ebf353b8a
thien <imagineworld14@gmail.com>
2023-03-30 22:04:47 +0700

    version2.1

--- admincp/index.php
+++ admincp/index.php
@@ -5,8 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admincp</title>
+    <link rel="stylesheet" type="text/css" href="css/styleadmincp.css">
 </head>
 <body>
-    <h3>Welcome to Admincp</h3>
+    <h3 class="title_admin">Welcome to Admincp</h3>
+    <div class="wrapper">
+        <?php
+            include("modules/header.php");
+            include("modules/menu.php");
+            include("modules/main.php");
+            include("modules/footer.php");
+        ?>
+    </div>
 </body>
 </html>
