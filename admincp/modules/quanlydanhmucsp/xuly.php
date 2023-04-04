99d172ab57caa7faedf156296e4f115446342462
thien <imagineworld14@gmail.com>
2023-04-04 23:07:32 +0700

    new update

---
+++ admincp/modules/quanlydanhmucsp/xuly.php
@@ -0,0 +1,10 @@
+<?php
+include('../../config/config.php');
+$tenloaisp = $_POST['tendanhmuc'];
+$thutu = $_POST['thutu'];
+if(isset($_POST['themdanhmuc'])){
+    $sql_them = "INSERT INTO danhmuc(tendanhmuc, thutu) VALUE('".$tenloaisp."','".$thutu."')";
+    mysqli_query($mysqli,$sql_them);
+    header('Location:../../index.php?acion=quanlydanhmucsanpham');
+}
+?>
